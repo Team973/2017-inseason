1157ac60948f9ce769d75583ed8cabc91794dc14
Andrew Nelson <andrew2nelson@gmail.com>
2017-02-16 13:19:46 -0800

    Implemented linearly interpolated lookup table

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -19,5 +19,23 @@ BOOST_AUTO_TEST_CASE(interp_lookup_table)
     InterpLookupTable lookup;
 
     lookup.AddPoint(5, 2);
-    BOOST_CHECK(lookup.LookupPoint(5) == -5);
+    BOOST_CHECK(lookup.LookupPoint(5) == 2);
+    BOOST_CHECK(lookup.LookupPoint(4) == 2);
+    BOOST_CHECK(lookup.LookupPoint(6) == 2);
+
+    lookup.AddPoint(6, 3);
+    BOOST_CHECK(lookup.LookupPoint(4) == 1);
+    BOOST_CHECK(lookup.LookupPoint(5) == 2);
+    BOOST_CHECK(lookup.LookupPoint(5.5) == 2.5);
+    BOOST_CHECK(lookup.LookupPoint(6) == 3);
+    BOOST_CHECK(lookup.LookupPoint(7) == 4);
+
+    lookup.AddPoint(7, 5);
+    BOOST_CHECK(lookup.LookupPoint(4) == 1);
+    BOOST_CHECK(lookup.LookupPoint(5) == 2);
+    BOOST_CHECK(lookup.LookupPoint(5.5) == 2.5);
+    BOOST_CHECK(lookup.LookupPoint(6) == 3);
+    BOOST_CHECK(lookup.LookupPoint(6.5) == 4);
+    BOOST_CHECK(lookup.LookupPoint(7) == 5);
+    BOOST_CHECK(lookup.LookupPoint(8) == 7);
 }
