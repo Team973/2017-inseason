e365d7c825ad329350650421180ade459a29aead
Andrew Nelson <andrew2nelson@gmail.com>
2017-02-21 14:23:05 -0800

    Set velocity measurement frame

--- src/subsystems/Shooter.cpp
+++ src/subsystems/Shooter.cpp
@@ -40,8 +40,8 @@ Shooter::Shooter(TaskMgr *scheduler, LogSpreadsheet *logger, CANTalon *leftAgita
     m_flywheelMotorPrimary->SetI(0.0);
     m_flywheelMotorPrimary->SetD(4.00);
     m_flywheelMotorPrimary->SetF(0.024);
-//    m_flywheelMotorPrimary->SetVelocityMeasurementPeriod(CANTalon::Period_20Ms);
-//    m_flywheelMotorPrimary->SetVelocityMeasurementWindow(32);
+    m_flywheelMotorPrimary->SetVelocityMeasurementPeriod(CANTalon::Period_20Ms);
+    m_flywheelMotorPrimary->SetVelocityMeasurementWindow(32);
 
     m_flywheelMotorReplica->ConfigNeutralMode(
             CANSpeedController::NeutralMode::kNeutralMode_Coast);
