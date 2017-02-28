8f4e55b62ccdd85ddc8cbbe949e0813c612dd509
Andrew Nelson <andrew2nelson@gmail.com>
2017-02-28 15:54:17 -0800

    Flywheel logging (included some logs; shouldn't leave these in repo for too long)

--- logs/graph.py
+++ logs/graph.py
@@ -10,12 +10,12 @@ import csv, os
 
 TEAM_NUMBER = 973
 
-time_window = 10 #time in seconds
+time_window = 20 #time in seconds
 points = []
 
 TIME_COL = 0
-#DATA_COLS = [16, 18, 19, 17] #flywheel tuning
-DATA_COLS = [5, 4, 7, 10]
+DATA_COLS = [17, 19, 18, 20] #flywheel tuning
+#DATA_COLS = [5, 4, 7, 10]
 GRAPHS = [["Left Encoder Rate", "Left motor signal (pow or vel)"], ["Left motor voltage"]]
 titles = []
 
@@ -27,10 +27,13 @@ with open(latest, "r") as f:
     titles = reader.__next__()
     print(list(zip(titles, range(999)))) # throw away title row
     for row in reader:
-        points.append(
-                tuple(
-                    [float(row[TIME_COL])] +
-                    [float(row[x]) for x in DATA_COLS]))
+        try:
+            points.append(
+                    tuple(
+                        [float(row[TIME_COL])] +
+                        [float(row[x]) for x in DATA_COLS]))
+        except:
+            print("Misread row")
 
 max_time = points[-1][0]
 min_time = max_time - time_window
@@ -39,13 +42,14 @@ print(min_time)
 
 x1 = [p[0] for p in points if p[0] >= min_time]
 y1 = [p[1] for p in points if p[0] >= min_time]
-y2 = [p[2]*2000 for p in points if p[0] >= min_time]
+y2 = [p[2] for p in points if p[0] >= min_time]
 y3 = [p[3] for p in points if p[0] >= min_time]
 y4 = [p[4] for p in points if p[0] >= min_time]
 
 plt.subplot(2, 1, 1)
 plt.plot(x1, y1, 'o-', label="speed")
 plt.plot(x1, y2, 'o-', label="setpoint")
+plt.ylim([2200, 3700])
 plt.subplot(2, 1, 2)
 plt.plot(x1, y3, 'o-', label="setpoint")
 plt.plot(x1, y4, 'o-', label="setpoint")
