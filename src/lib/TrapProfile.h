772238fbe11ae7c962efa2c805735c0e2d4c019e
Andrew Nelson <andrew2nelson@gmail.com>
2017-04-04 12:23:17 -0700

    Refactored trap profile to have a static checked and a runtime checked version (for unit testing) and added integral checking unit test (translated directly from python)

--- src/lib/TrapProfile.h
+++ src/lib/TrapProfile.h
@@ -2,30 +2,38 @@
 
 #include "lib/util/Util.h"
 #include "stdio.h"
+#include <math.h>
 
 namespace frc973 {
 
 namespace Profiler {
 
 struct Waypoint {
-    const double time;
+    double time;
 
-    const double linear_dist;
-    const double linear_vel;
+    double linear_dist;
+    double linear_vel;
 
-    const double angular_dist;
-    const double angular_vel;
+    double angular_dist;
+    double angular_vel;
 
-    const bool done;
+    bool done;
+    bool error;
 
-    Waypoint(double time_, double linear_dist_, double linear_vel_,
-            double angular_dist_, double angular_vel_, bool done_)
+    Waypoint(double time_,
+            double linear_vel_,
+            double linear_dist_,
+            double angular_vel_,
+            double angular_dist_,
+            bool done_,
+            bool error_)
          : time(time_)
          , linear_dist(linear_dist_)
          , linear_vel(linear_vel_)
          , angular_dist(angular_dist_)
          , angular_vel(angular_vel_)
-         , done(done_) {
+         , done(done_)
+         , error(error_) {
     }
 };
 
@@ -40,25 +48,44 @@ struct FakeFloat {
     static constexpr double value = static_cast<double>(N) / static_cast<double>(D);
 };
 
+/**
+ * TrapProfileUnsafe does the calculation at runtime like one would expect
+ * and is a normal function.  Do not call this function directly, it is
+ * dangerous.  Instead, call TrapProfile.
+ */
+Waypoint TrapProfileUnsafe(double time,
+        double distance, double angle,
+        double max_velocity, double max_acceleration,
+        bool end_halt, bool start_halt);
+
+/**
+ * Safely generates a trapazoidal motion profile.  Checks at compile time
+ * for profile safety.
+ */
 template<typename DISTANCE, typename ANGLE,
         typename MAX_VELOCITY, typename MAX_ACCELERATION,
-        bool END_HALT, bool START_HALT>
+        bool START_HALT, bool END_HALT>
 Waypoint TrapProfile(double time) {
-    static constexpr double distance = DISTANCE::value;
-    static constexpr double angle = ANGLE::value;
-    static constexpr double max_velocity = MAX_VELOCITY::value;
-    static constexpr double max_acceleration = MAX_ACCELERATION::value;
-    static constexpr bool end_halt = END_HALT;
-    static constexpr bool start_halt = START_HALT;
+    constexpr double distance = DISTANCE::value;
+    constexpr double angle = ANGLE::value;
+    constexpr double max_velocity = MAX_VELOCITY::value;
+    constexpr double max_acceleration = MAX_ACCELERATION::value;
+    constexpr bool start_halt = START_HALT;
+    constexpr bool end_halt = END_HALT;
+
+    constexpr double dist_ramp = 0.5 * max_velocity *
+        max_velocity / max_acceleration * Util::signum(distance);
 
     static_assert(
             !(!START_HALT && END_HALT) ||
-            0.5 * MAX_VELOCITY::value * MAX_VELOCITY::value /
-            MAX_ACCELERATION::value < Util::abs(DISTANCE::value),
+            dist_ramp < Util::abs(DISTANCE::value),
             "Profile is over-constrained");
 
-    return Waypoint(time, 0.0, 0.0, 0.0, 0.0, false);
-};
+    return TrapProfileUnsafe(time,
+            distance, angle,
+            max_velocity, max_acceleration,
+            start_halt, end_halt);
+}
 
 }
 
