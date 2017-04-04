28c5b8a0a48458a0f9d50a26fb4981fca5d2831e
Andrew Nelson <andrew2nelson@gmail.com>
2017-04-04 14:10:11 -0700

    Added trap drive controller class

---
+++ src/controllers/TrapDriveController.h
@@ -0,0 +1,54 @@
+/*
+ * TrapDriveController.h
+ */
+
+#pragma once
+
+#include "lib/DriveBase.h"
+#include "lib/filters/PID.h"
+#include <stdio.h>
+
+using namespace frc;
+
+namespace frc973 {
+
+class TrapDriveController: public DriveController {
+public:
+	TrapDriveController(DriveStateProvider *state);
+	virtual ~TrapDriveController();
+
+    void SetTarget(DriveBase::RelativeTo relativeTo,
+            double dist, double angle,
+            bool start_halt = true, bool end_halt = true);
+
+	void CalcDriveOutput(DriveStateProvider *state,
+			DriveControlSignalReceiver *out) override;
+
+	bool OnTarget() override { return m_done; }
+
+	void Start() override;
+
+	void Stop() override;
+private:
+    double DistFromStart();
+    double AngleFromStart();
+
+    DriveStateProvider *m_state;
+    double m_dist, m_angle;
+    double m_dist_offset, m_angle_offset, m_time_offset;
+    bool m_start_halt, m_end_halt;
+
+    /* pid for linear {pos,vel} */
+    PID m_l_pos_pid, m_l_vel_pid;
+
+    /* pid for angular {pos,vel} */
+    PID m_a_pos_pid, m_a_vel_pid;
+
+    bool m_done;
+    bool m_needSetControlMode;
+
+    static constexpr double MAX_VELOCITY = 130;     //in/sec
+    static constexpr double MAX_ACCELERATION = 10.0; //in/sec^2
+};
+
+}
