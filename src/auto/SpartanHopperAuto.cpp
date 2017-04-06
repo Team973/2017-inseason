ffcdb1494806c8badcd8c9df3371eaa6ecf67956
Andrew Nelson <andrew2nelson@gmail.com>
2017-04-06 14:55:24 -0700

    copied the 971 auto going to the near hopper

--- src/auto/SpartanHopperAuto.cpp
+++ src/auto/SpartanHopperAuto.cpp
@@ -1,134 +1,138 @@
 #include "Robot.h"
 #include "AutoCommon.h"
+#include "lib/TrapProfile.h"
 
 namespace frc973 {
     
+/**
+ * This auto routine must start with the hopper away from the wall
+ * because we move forward.
+ */
 void Robot::SpartanHopperAuto(){
-  switch(m_autoState){
-    case 0:
-      m_boilerPixy->Enable();
-      m_drive->PIDDrive(-55.5, 0.0, DriveBase::RelativeTo::Now, 1.0);
-      m_gearIntake->SetPickUpManual();
-      m_gearIntake->SetGearPos(GearIntake::GearPosition::down);
-      m_shooter->SetFlywheelSpeed(SHOOTER_RPM);
-      m_shooter->StopAgitator();
-      m_shooter->StartConveyor(0.0);
-      m_autoTimer = GetMsecTime();
-      m_autoState++;
-      break;
-    case 1:
-      if (GetMsecTime() - m_autoTimer > 250) {
-          m_gearIntake->SetGearPos(GearIntake::GearPosition::up);
-      }
-      if(m_drive->OnTarget() || GetMsecTime() - m_autoTimer >= 2000){
-          m_drive->PIDTurn(-31.5 * m_autoDirection, DriveBase::RelativeTo::Absolute, 1.0)
-              ->SetAngleTolerance(10.0, 2.0);
-          m_autoTimer = GetMsecTime();
-          m_autoState++;
-      }
-      break;
-    case 2:
-        if (m_drive->OnTarget() || GetMsecTime() - m_autoTimer >= 2000) {
-            double boilerOffset = m_boilerPixy->GetXOffset() *
-                BoilerPixy::PIXY_OFFSET_CONSTANT;
-            if (Util::abs(boilerOffset) >= 10.0) {
-                //it's too big so screw it
+    double initial_dist = 48.0;
+
+    if(m_alliance == Alliance::Red){
+        initial_dist += 0.0;
+    }
+    else{
+        initial_dist += 0.0;
+    }
+
+    switch (m_autoState){
+        case 0:
+            m_compressor->Disable();
+            m_ballIntake->ExpandHopper();
+            m_shooter->SetFlywheelSpeed(3030);
+            m_gearIntake->SetPickUpManual();
+            m_gearIntake->SetGearPos(GearIntake::GearPosition::down);
+            m_shooter->StopAgitator();
+            m_shooter->StartConveyor(0.0);
+            m_drive
+                ->TrapDrive(DriveBase::RelativeTo::Now, initial_dist, 0.0)
+                ->SetHalt(true, false)
+                ->SetConstraints(80.0, 48.0);
+            m_autoState++;
+            break;
+        case 1:
+            if (GetMsecTime() - m_autoTimer > 250) {
+                m_gearIntake->SetGearPos(GearIntake::GearPosition::up);
+            }
+            if (m_drive->OnTarget()) {
+                m_gearIntake->SetGearPos(GearIntake::GearPosition::up);
+                m_drive
+                    ->TrapDrive(DriveBase::RelativeTo::Now, 3.0 * 12.0,
+                                m_autoDirection * -90.0)
+                    ->SetHalt(false, false)
+                    ->SetConstraints(80.0, 48.0);
                 m_autoState++;
             }
-            else {
+            break;
+        case 2:
+            if (m_drive->OnTarget()) {
+                /*
+                 * the TrapProfile template is a compile-time hack to make
+                 * sure the profile won't mess up at runtime.  We aren't
+                 * going to follow the whole thing; we're going to switch to
+                 * a regular drive after a few inches.  TrapDrive just makes
+                 * absolute certain that we don't have angular momentum
+                 * when we transition to that regular arcade drive.  The
+                 * arcade drive will press against the hopper.
+                 */
+                using namespace Profiler;
+                TrapProfile<FakeFloat<80>, FakeFloat<0>,
+                    FakeFloat<80>, FakeFloat<48>,
+                    false, true>(0);
                 m_drive
-                    ->PIDTurn(m_drive->GetAngle() - boilerOffset,
-                               DriveBase::RelativeTo::Absolute, 1.0)
-                    ->SetAngleTolerance(0.0, 0.0);
+                    ->TrapDrive(DriveBase::RelativeTo::Now, 80.0, 0.0)
+                    ->SetHalt(false, true)
+                    ->SetConstraints(80.0, 48.0);
+                m_ballIntake->BallIntakeStart();
                 m_autoTimer = GetMsecTime();
                 m_autoState++;
             }
-          }
-        break;
-    case 3:
-        if ((m_drive->OnTarget() && m_shooter->OnTarget()) ||
-                GetMsecTime() - m_autoTimer > 3000) {
-            m_shooter->SetShooterState(Shooter::ShootingSequenceState::manual);
-            m_shooter->StartAgitator(1.0, Shooter::Side::right);
-            m_shooter->StartAgitator(1.0, Shooter::Side::left);
-            m_shooter->StartConveyor(1.0);
-            m_autoTimer = GetMsecTime();
-            m_autoState++;
-        }
-        break;
-    case 4:
-        if(GetMsecTime() - m_autoTimer >= 2500){
-          m_drive->PIDTurn(-60.0 * m_autoDirection, DriveBase::RelativeTo::Absolute, 1.0)
-              ->SetAngleTolerance(10.0, 2.0);
-          m_shooter->StopAgitator();
-          m_shooter->StartConveyor(0.0);
-          m_autoTimer = GetMsecTime();
-          m_autoState++;
-        }
-        break;
-    case 5:
-      if(m_drive->OnTarget() || GetMsecTime() - m_autoTimer >= 1500){
-        m_drive->PIDDrive(-50.0, 0.0, DriveBase::RelativeTo::Now, 1.0);
-        m_autoState++;
-      }
-      break;
-    case 6:
-        if (m_drive->OnTarget()) {
-          m_drive->PIDTurn(-60.0 * m_autoDirection, DriveBase::RelativeTo::Absolute, 1.0);
-          m_autoState++;
-        }
-        break;
-    case 7:
-        if (m_drive->OnTarget() || GetMsecTime() - m_autoTimer >= 1500) {
-            double gearOffset = m_pixyR->GetOffset() *
-                PixyThread::GEAR_DEGREES_PER_PIXEL;
-
-            if (Util::abs(gearOffset) >= 10.0) {
+            break;
+        case 3:
+            /*
+             * Advance after 18 inches of trap driving.  We can generalize
+             * the trap profiler in offseason to exit on arbitrary velocity
+             * but for now we have to make an extra long profile and exit
+             * early.
+             */
+            if (GetMsecTime() - m_autoTimer > 700 &&
+                    m_drive->GetTrapDriveController()
+                           ->DistFromStart() > 18.0) {
+                m_drive->ArcadeDrive(0.1, 0.0);
+                m_autoTimer = GetMsecTime();
                 m_autoState++;
             }
-            else {
-              m_drive
-                  ->PIDTurn(m_drive->GetAngle() - gearOffset,
-                             DriveBase::RelativeTo::Absolute, 1.0)
-                  ->SetDistTolerance(15.0, 25.0)
-                  ->SetAngleTolerance(30.0, 60.0);
+            break;
+        case 4:
+            if (GetMsecTime() - m_autoTimer > 2500) {
+                m_drive
+                    ->TrapDrive(DriveBase::RelativeTo::Now, -24.0,
+                                m_autoDirection * 90.0)
+                    ->SetHalt(true, true)
+                    ->SetConstraints(80.0, 48.0);
+                m_autoTimer = GetMsecTime();
+                m_autoState++;
+            }
+            break;
+        case 5:
+            if (m_drive->OnTarget() || GetMsecTime() - m_autoTimer >= 2000) {
+                m_ballIntake->BallIntakeStop();
+                double angleOffset = m_boilerPixy->GetXOffset() *
+                    BoilerPixy::PIXY_OFFSET_CONSTANT;
+                if (Util::abs(angleOffset) >= 10.0) {
+                    m_autoState++;
+                }
+                else {
+                    m_drive
+                        ->PIDTurn(m_drive->GetAngle() - angleOffset,
+                                   DriveBase::RelativeTo::Absolute, 1.0)
+                        ->SetDistTolerance(15.0, 25.0)
+                        ->SetAngleTolerance(30.0, 60.0);
+                    m_autoTimer = GetMsecTime();
+                    m_autoState++;
+                }
+            }
+            break;
+        case 6:
+            if (m_drive->OnTarget() || (GetMsecTime() - m_autoTimer >= 1000 )) {
               m_autoTimer = GetMsecTime();
               m_autoState++;
             }
-        }
-        break;
-    case 8:
-        if (m_drive->OnTarget()) {
-            m_autoTimer = GetMsecTime();
-            m_drive->ArcadeDrive(-0.3, 0.0);
-            m_autoState++;
-        }
-        break;
-    case 9:
-        if (m_gearIntake->IsGearReady()) {
-            //hit the gear, continue normally
-            m_drive->PIDDrive(30.0, 0.0, DriveBase::RelativeTo::Now, 0.8);
-            m_gearIntake->SetGearPos(GearIntake::GearPosition::down);
-            m_gearIntake->SetGearIntakeState(GearIntake::GearIntakeState::released);
-            m_autoState++;
-        }
-        else if (GetMsecTime() - m_autoTimer > 3000) {
-            //we did not hit it after 3 seconds so back up and try again
-            m_drive->PIDDrive(20.0, 0.0, DriveBase::RelativeTo::Now, 0.8);
-            m_autoState = 6;
-        }
-        break;
-    case 10:
-        //should be done scoring gear... make hair merry red left
-        if (m_drive->OnTarget()) {
-            m_drive->PIDTurn(-90.0 * m_autoDirection, DriveBase::RelativeTo::SetPoint, 0.8);
-            m_autoState++;
-        }
-        break;
-    default:
-        break;
-  }
+            break;
+        case 7:
+              m_ballIntake->RetractHopper();
+              m_shooter->SetShooterState(Shooter::ShootingSequenceState::manual);
+              m_shooter->StartConveyor(0.9);
+              m_shooter->StartAgitator(1.0, Shooter::Side::right);
+              m_shooter->StartAgitator(1.0, Shooter::Side::left);
+              m_autoState++;
+            break;
+        case 8:
+            break;
+    }
 }
 
 }
-
